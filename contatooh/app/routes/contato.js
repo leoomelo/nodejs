a00ff638835718a0dd6108717d9233d76f5e6d20
Leonardo Melo <lom@cesar.org.br>
2015-10-06 17:37:37 -0300

    commit inicial

---
+++ contatooh/app/routes/contato.js
@@ -0,0 +1,5 @@
+module.exports = function(app) {
+	var controller = app.controllers.contato;
+	app.get('/contatos', controller.listaContatos);
+	app.get('/contato/{id}, controller.getContatoById);
+};
