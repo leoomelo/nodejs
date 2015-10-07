33acbfca60bac00deef97b1511c5cfe6bfc05032
Leonardo Melo <lom@cesar.org.br>
2015-10-07 17:13:37 -0300

    angular até ng-route

--- contatonode/app/routes/contato.js
+++ contatonode/app/routes/contato.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
 	var controller = app.controllers.contato;
 	app.get('/contatos', controller.listaContatos);
-	app.get('/contato/{id}, controller.getContatoById);
+	app.get('/contatos/:id', controller.obtemContato);
 };
--- contatonode/config/express.js
+++ contatonode/config/express.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
+
 module.exports = function() {
 	var app = express();
 	app.get('/');
@@ -7,6 +9,9 @@ module.exports = function() {
 	app.use(express.static('./public'));
 	app.set('view engine', 'ejs');
 	app.set('views', './app/views');
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
 	load('models', {cwd: 'app'})
 	  .then('controllers')
 	  .then('routes')
