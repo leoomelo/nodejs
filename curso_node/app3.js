1360efb6363ff682d73e221dbc6c92435133f8f4
Leonardo Melo <leoomelo@Leonardos-MacBook-Pro.local>
2016-11-20 17:36:03 -0300

    modularizando a aplicação

--- curso_node/app3.js
+++ curso_node/app3.js
@@ -1,19 +1,10 @@
-var express = require('express');
-var app = express();
+var app = require('./config/server');
 
-app.set('view engine', 'ejs');
+var rotaHome = require('./app/routes/home')(app);
 
-app.get('/', function(req, res) {
-  res.render("home/index")
-});
+var rotasNoticias = require('./app/routes/noticias')(app);
 
-app.get('/formulario_inclusao_noticia', function(req, res) {
-  res.render("admin/form_add_noticia")
-});
-
-app.get('/noticias', function(req, res) {
-  res.render("noticias/noticias")
-});
+var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function() {
   console.log('Servidor rodando com express agora');
