3f7350cfe4e4a14f4dadcf93d16dd7220713adec
Leonardo Melo <lom@cesar.org.br>
2016-11-22 14:34:27 -0300

    encapsulated database connection

--- curso_node/app.js
+++ curso_node/app.js
@@ -1,10 +1,5 @@
 var app = require('./config/server');
-
-var rotaHome = require('./app/routes/home')(app);
-
-var rotasNoticias = require('./app/routes/noticias')(app);
-
-var rotaFormInclusaoNoticia = require('./app/routes/formulario_inclusao_noticia')(app);
+var consign = require('consign');
 
 app.listen(3000, function() {
   console.log('Servidor rodando com express agora');
--- curso_node/config/dbConnection.js
+++ curso_node/config/dbConnection.js
@@ -1,12 +1,14 @@
 var mysql = require('mysql');
 
-module.exports = function() {
-
+var connMysql = function() {
   return mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
     database: 'portal_noticias'
   });
+};
 
+module.exports = function() {
+  return connMysql;
 }
