9c752cb8b0f2b7a1cbf113e0167c21bab77d1751
Leonardo Melo <lom@cesar.org.br>
2016-11-22 13:56:12 -0300

    refactory database connection

--- curso_node/app/routes/noticias.js
+++ curso_node/app/routes/noticias.js
@@ -1,14 +1,10 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app) {
 
+  var connection = dbConnection();
+  
   app.get('/noticias', function(req, res) {
-
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: 'root',
-      database: 'portal_noticias'
-    });
 
     connection.query("select * from noticias", function(error, result) {
       res.render("noticias/noticias", {noticias : result});
