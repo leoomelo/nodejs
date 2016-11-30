7323fc96f6e9cdcce983e0bcb0af2de128008c21
Leonardo Melo <lom@cesar.org.br>
2016-11-30 10:31:32 -0300

    route config for noticia and noticias.js. Form to include new noticia

--- curso_node/app/routes/noticia.js
+++ curso_node/app/routes/noticia.js
@@ -3,7 +3,9 @@ module.exports = function(app) {
   app.get('/noticia', function(req, res) {
 
     var connection = app.config.dbConnection();
-    connection.query("select * from noticias where id_noticia = 1;", function(err, result){
+    var noticiasModel = app.app.models.noticiasModel;
+
+    noticiasModel.getNoticia(connection, function(err, result){
       res.render("noticias/noticia", {noticia : result[0]});
     })
 
--- curso_node/app/routes/noticias.js
+++ curso_node/app/routes/noticias.js
@@ -2,7 +2,9 @@ module.exports = function(app) {
 
   app.get('/noticias', function(req, res) {
     var connection = app.config.dbConnection();
-    connection.query("select * from noticias", function(error, result) {
+    var noticiasModel = app.app.models.noticiasModel;
+
+    noticiasModel.getNoticias(connection, function(error, result) {
       res.render("noticias/noticias", {noticias : result});
     });
   });
