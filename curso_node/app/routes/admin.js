963f356a443cee0ebe4d527d8f3efa87d1b76afc
Leonardo Melo <lom@cesar.org.br>
2016-11-30 13:07:32 -0300

    rename NoticiasModel para NoticiasDAO

--- curso_node/app/routes/admin.js
+++ curso_node/app/routes/admin.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
   app.post('/noticias/salvar', function(req, res) {
     var noticia = req.body;
     var connection = app.config.dbConnection();
-    var noticiasModel = new app.app.models.noticiasModel(connection);
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
     noticiasModel.salvarNoticia(noticia, function(err, result) {
       res.redirect('/noticias');
--- curso_node/app/routes/noticia.js
+++ curso_node/app/routes/noticia.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.get('/noticia', function(req, res) {
 
     var connection = app.config.dbConnection();
-    var noticiasModel = new app.app.models.noticiasModel(connection);
+    var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
     noticiasModel.getNoticia(function(err, result){
       res.render("noticias/noticia", {noticia : result[0]});
