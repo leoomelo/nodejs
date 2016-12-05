d0e8a41f85629c28549af0787863f13cfda621d6
Leonardo Melo <lom@cesar.org.br>
2016-12-05 16:49:41 -0300

    validating data and restore values from fields typed before

--- curso_node/app/routes/admin.js
+++ curso_node/app/routes/admin.js
@@ -1,14 +1,31 @@
 module.exports = function(app) {
   app.get('/formulario_inclusao_noticia', function(req, res) {
-    res.render("admin/form_add_noticia")
+    res.render("admin/form_add_noticia", {validacao: {}, noticia: {} })
   });
 
   app.post('/noticias/salvar', function(req, res) {
     var noticia = req.body;
+
+    req.assert('titulo','Título é obrigatório').notEmpty();
+    req.assert('resumo','Resumo é obrigatório').notEmpty();
+    req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+    req.assert('autor','Autor é obrigatório').notEmpty();
+    req.assert('data_noticia','Data da notícia é obrigatória').notEmpty()
+    req.assert('data_noticia', 'Data da Notícia está no formato errado').isDate({format: 'YYYY-MM-DD'});
+    req.assert('noticia','Notícia é obrigatória').notEmpty();
+
+    var erros = req.validationErrors();
+
+    if (erros) {
+      res.render('admin/form_add_noticia', {validacao : erros, noticia : noticia});
+      return;
+    }
+
     var connection = app.config.dbConnection();
     var noticiasModel = new app.app.models.NoticiasDAO(connection);
 
     noticiasModel.salvarNoticia(noticia, function(err, result) {
+
       res.redirect('/noticias');
     });
   })
