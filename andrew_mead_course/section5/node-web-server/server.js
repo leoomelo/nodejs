3341b90cc164fc6c3fd6ca50bcf8f1754e0d138e
Leonardo Oliveira de Melo <lom@CESAR-LDAP.INTRANET>
2017-08-14 17:26:36 -0300

    começando a trabalhar com handlebars (include, helpers)

--- andrew_mead_course/section5/node-web-server/server.js
+++ andrew_mead_course/section5/node-web-server/server.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+})
+
 app.get('/', (req, res) => {
-	//res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'Leonardo',
-		likes: [
-			'Guitar',
-			'Cities',
-			'Ron']
-	});
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome to my home page'
+	})
 });
 
 app.get('/about', (req, res) => {
-	res.send('About page');
+	res.render('about.hbs', {
+		pageTitle: 'About Page'		
+	});
 });
 
 app.get('/bad', (req, res) => {
@@ -23,4 +34,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
